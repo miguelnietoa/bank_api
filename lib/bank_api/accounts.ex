152b31f7d8b998ff9c7277c09d891dabe5d68c2e
Miguel Nieto A <39246879+miguelnietoa@users.noreply.github.com>
2022-08-05 16:49:21 -0500

    Add middleware to ValidateCommand

--- lib/bank_api/accounts.ex
+++ lib/bank_api/accounts.ex
@@ -5,52 +5,41 @@ defmodule BankAPI.Accounts do
 
   import Ecto.Query, warn: false
 
-  alias Ecto.Changeset
   alias BankAPI.Repo
   alias BankAPI.Router
   alias BankAPI.Accounts.Commands.OpenAccount
   alias BankAPI.Accounts.Projections.Account
 
+  def uuid_regex do
+    ~r/[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}/
+  end
+
   def get_account(uuid), do: Repo.get!(Account, uuid)
 
-  def open_account(account_params) do
-    changeset = account_opening_changeset(account_params)
+  def open_account(%{"initial_balance" => initial_balance}) do
+    account_uuid = UUID.uuid4()
 
-    if changeset.valid? do
-      account_uuid = UUID.uuid4()
+    dispatch_result =
+      %OpenAccount{
+        initial_balance: initial_balance,
+        account_uuid: account_uuid
+      }
+      |> Router.dispatch(application: BankAPI.CommandedApplication)
 
-      dispatch_result =
-        %OpenAccount{
-          initial_balance: changeset.changes.initial_balance,
-          account_uuid: account_uuid
-        }
-        |> Router.dispatch(application: BankAPI.CommandedApplication)
-
-      case dispatch_result do
-        :ok ->
-          {
-            :ok,
-            %Account{
-              uuid: account_uuid,
-              current_balance: changeset.changes.initial_balance
-            }
+    case dispatch_result do
+      :ok ->
+        {
+          :ok,
+          %Account{
+            uuid: account_uuid,
+            current_balance: initial_balance
           }
+        }
 
-        reply ->
-          reply
-      end
-    else
-      {:validation_error, changeset}
+      reply ->
+        reply
     end
   end
 
-  defp account_opening_changeset(params) do
-    {
-      params,
-      %{initial_balance: :integer}
-    }
-    |> Changeset.cast(params, [:initial_balance])
-    |> Changeset.validate_required([:initial_balance])
-    |> Changeset.validate_number(:initial_balance, greater_than: 0)
-  end
+  def open_account(_params), do: {:error, :bad_command}
 end
--- lib/bank_api/accounts/commands/open_account.ex
+++ lib/bank_api/accounts/commands/open_account.ex
@@ -1,5 +1,25 @@
 defmodule BankAPI.Accounts.Commands.OpenAccount do
+  use Ecto.Schema
+  import Ecto.Changeset
+
+  alias BankAPI.Accounts
+
   @enforce_keys [:account_uuid]
 
-  defstruct [:account_uuid, :initial_balance]
+  embedded_schema do
+    field :account_uuid
+    field :initial_balance, :integer
+  end
+
+  def validate(command) do
+    command
+    |> change()
+    |> validate_required([:account_uuid, :initial_balance])
+    |> validate_format(:account_uuid, Accounts.uuid_regex())
+    |> validate_number(:initial_balance, greater_than: 0)
+    |> case do
+      %{valid?: true} -> :ok
+      %{valid?: false, errors: errors} -> {:error, errors}
+    end
+  end
 end
